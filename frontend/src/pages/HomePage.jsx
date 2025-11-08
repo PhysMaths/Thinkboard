75f6ddebc6aaf772ab31025d00c1dce09aa3a887
Allen Jolly Mathew <allenjollymathew@Allens-MacBook-Air.local>
2025-11-08 14:36:12 +0000

    updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -39,7 +40,13 @@ const HomePage = () => {
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>Loading Notes...</div> }
         {notes.length > 0 && !isRateLimited &&  (
-          
+          <div className='grid gridcols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map(note => (
+              <div>
+                <NoteCard key={note._id} note={note}></NoteCard>
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
