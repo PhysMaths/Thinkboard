ac5148c31e1cad7080f2bc461831009aa594f476
Allen Jolly Mathew <allenjollymathew@Allens-MacBook-Air.local>
2025-11-08 14:35:57 +0000

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,27 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
-connectDB();
-
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    });
 });
 
 
